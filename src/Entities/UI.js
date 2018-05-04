e047006f75dbd6ecc14477b96835446ff5a81881
Christopher Kinch <Christopher_Kinch@dennis.co.uk>
2018-05-04 18:16:31 +0100

    First pass UI added

--- src/Entities/UI.js
+++ src/Entities/UI.js
@@ -2,19 +2,22 @@ class UI extends Phaser.GameObjects.Container {
 
 	constructor(scene) {
 		super(scene, 0, 0);
+		this.scene.add.existing(this);
 
-		this.spells = 3;
-		this.spacing = 60;
+		this.spells = 5;
+		this.spacing = 70;
 		this.frames = [];
+
+		this.setSpellFrames();
 	}
 
 	setSpellFrames(){
+		let x = Phaser.Display.Bounds.GetLeft(this.scene.zone);
+		let y = Phaser.Display.Bounds.GetBottom(this.scene.zone);
 		for(let i=0; i<this.spells; i++) {
-			this.frames.push(
-				this.scene.add.sprite(0, 0, 'icon', 'frame').setDepth(this.scene.depth_group.BASE).setScale(2);
-			)
-			Phaser.Display.Align.In.BottomLeft(this.button, this.scene.zone);
-			this.frames[i].x = this.spacing*i;
+			let frame = this.scene.add.sprite(x + (this.spacing*i), y, 'icon', 'icon_blank').setOrigin(0.25,0.75).setDepth(this.scene.depth_group.BASE).setScale(1.8).setAlpha(0.3);
+			this.add(frame);
+			this.frames.push(frame);
 		}
 	}
 
