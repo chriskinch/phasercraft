58050243a2a0d70e63dcc03dee8059c4038d35bf
chriskinch <kinchie@gmail.com>
2019-08-25 12:19:04 +0100

    #12 Toggle menu

--- src/UI/Menu.js
+++ src/UI/Menu.js
@@ -9,14 +9,26 @@ class Menu extends Phaser.GameObjects.Container {
 		this.scene.add.existing(this).setDepth(this.scene.depth_group.UI);
 		
 		console.log("MENU")
+		this.scene.input.keyboard.on(`keydown-S`, this.toggleMenu, this); 
 
 		// const menu = this.scene.add.sprite(10, 20, 'menu-ui');
+	}
 
-		const menu = this.scene.add.ninePatch(0, 0, 500, 400, "menu-background", null, {
-			top: 14
-		});
-
+	setMenu() {
+		const menu = this.scene.add.ninePatch(0, 0, 500, 400, "menu-background", null, { top: 14 }).setDepth(this.scene.depth_group.TOP);
 		Phaser.Display.Align.In.Center(menu, this.scene.zone);
+
+		return menu;
+	}
+
+	removeMenu() {
+		this.menu.destroy();
+		return null;
+	}
+
+	toggleMenu() {
+		console.log(this.menu);
+		this.menu = (this.menu) ? this.removeMenu() : this.setMenu(); 
 	}
 }
 
