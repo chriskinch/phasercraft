433df45286346654f551a15e4d0f709905913acf
Chris Kinch <kinchie@gmail.com>
2019-10-01 11:05:11 +0100

    Fix and improvement to snaretrap

--- src/Entities/Spells/SnareTrap.js
+++ src/Entities/Spells/SnareTrap.js
@@ -21,12 +21,10 @@ class SnareTrap extends Spell {
 		// This is what the spell scales from.
         this.power = this.player.stats.magic_power;
     }
-    
-    animation() {
-        const pointer = this.scene.input.activePointer;
-        this.item = new Trap(this.scene, pointer.x, pointer.y, this.lifespan);
-        this.item.once('trap:collide', this.effect, this);
-    }
+
+    // Override and remove the defaul spell animation functions.
+    setAnimation(){}
+    startAnimation() {}
 
 	setCastEvents(state){
 		// Elegible targets for this spell
@@ -37,20 +35,30 @@ class SnareTrap extends Spell {
 		this.scene.events[state]('pointerdown:player', this.clearSpell, this);
 	}
 
-	effect(target){
-        if(target) {
-            target.body.setMaxVelocity(0);
-            target.monster.anims.pause();
-            target.body.checkCollision.none = true;
-            // Using a flat value and false so trap annot crit. TODO: Bleed over time.
-            target.health.adjustValue(-20, this.type, false);
-            
-            this.scene.time.delayedCall(this.duration * 1000, () => {
-                target.body.setMaxVelocity(100);
-                target.monster.anims.resume();
-                target.body.checkCollision.none = false;
-            }, [], this);
-        }
+    triggerTrap(target) {
+        target.body.setMaxVelocity(0);
+        target.monster.anims.pause();
+        target.body.checkCollision.none = true;
+        // Using a flat value and false so trap annot crit. TODO: Bleed over time.
+        target.health.adjustValue(-20, this.type, false);
+        
+        this.scene.time.delayedCall(this.duration * 1000, () => {
+            target.body.setMaxVelocity(100);
+            target.monster.anims.resume();
+            target.body.checkCollision.none = false;
+        }, [], this);
+    }
+    
+    layTrap() {
+        const pointer = this.scene.input.activePointer;
+        this.item = new Trap(this.scene, pointer.x, pointer.y, this.lifespan);
+        this.item.once('trap:collide', this.effect, this);
+    }
+
+    effect(target){
+        // Only trigger if the target have a body.
+        // Game scene does not so lay the trap rather then trigger the trap.
+        (target.body) ? this.triggerTrap(target) : this.layTrap();
     }
 }
 
