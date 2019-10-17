adddb36849989b6143765bb529ca8b5dee7aa6d4
Chris Kinch <kinchie@gmail.com>
2019-10-17 12:30:45 +0100

    Package tweaks

--- src/app.js
+++ src/app.js
@@ -8,12 +8,6 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
-// import 'phaser';
-// import LoadScene from './Scenes/LoadScene';
-// import GameScene from './Scenes/GameScene';
-// import GameOverScene from './Scenes/GameOverScene';
-// import SelectScene from './Scenes/SelectScene';
-
 document.body.setAttribute("style", "margin:0;");
 
 class App extends Component {
