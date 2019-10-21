5d389f3c01273bdba3391a89d750348941c58481
chriskinch <kinchie@gmail.com>
2019-10-21 15:18:47 +0100

    Adding equip slots

---
+++ src/UI/components/atoms/Slot.js
@@ -0,0 +1,10 @@
+import React from "react";
+import 'styled-components/macro';
+
+const Slot = ({ slot }) => {
+    return (
+        <div>{slot}</div>
+    );
+}
+
+export default Slot;
--- src/UI/components/organisms/Equipment.js
+++ src/UI/components/organisms/Equipment.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import 'styled-components/macro';
+import Slot from '../atoms/Slot';
 import Stats from "../molecules/Stats";
 
 const Equipment = (props) => {
     const { character, stats } = props;
     return (
         <>
-            <h2>Level 1</h2>
-            <img 
-                src={`UI/player/${character}.gif`}
-                alt="This is you!"
-                css={`
-                    height: 3em;
-                `}
-            />
-            <Stats>
-                { stats }
-            </Stats>
+            <section>
+                <h2>Level 1</h2>
+                <img 
+                    src={`UI/player/${character}.gif`}
+                    alt="This is you!"
+                    css={`
+                        height: 3em;
+                    `}
+                />
+                <Stats>
+                    { stats }
+                </Stats>
+            </section>
+            <section>
+                <Slot slot="Helm" />
+                <Slot slot="Body" />
+                <Slot slot="Weapon" />
+                <Slot slot="Amulet" />
+                <Slot slot="Ring" />
+            </section>
         </>
     );
 }
