1e29bcb8625fc502d29df8c4180b2d051b24ad6d
Chris Kinch <kinchie@gmail.com>
2019-12-23 18:01:28 +0000

    Enzyme unit test example for button

--- src/UI/components/atoms/Button/Button.test.js
+++ src/UI/components/atoms/Button/Button.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
 import Button from './index';
 
-it('renders correctly', () => {
-    const tree = renderer
-    .create(<Button text="Button Title" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+configure({ adapter: new Adapter() });
+
+describe('Test Button component', () => {
+    it('renders correctly', () => {
+        const tree = renderer
+            .create(<Button text="Button Title" />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('has a working onClick', () => {
+        const mockCallBack = jest.fn();
+        const button = mount(<Button text="Button with onClick" onClick={mockCallBack} />);
+        button.find('button').simulate('click');
+        expect(mockCallBack.mock.calls.length).toEqual(1);
+    });
 });
