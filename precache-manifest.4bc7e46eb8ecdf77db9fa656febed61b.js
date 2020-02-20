8438db265f20a2d3f7eb47b13b6ce6ead731db5f
Chris Kinch <kinchie@gmail.com>
2020-02-20 13:38:50 +0000

    Updates

---
+++ precache-manifest.4bc7e46eb8ecdf77db9fa656febed61b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07eb1eaea34b3eb149ca9ca2fa222ff1",
+    "url": "/phasercraft/index.html"
+  },
+  {
+    "revision": "f2d47e79505e94c828e3",
+    "url": "/phasercraft/static/css/main.15569215.chunk.css"
+  },
+  {
+    "revision": "ad999376b6b40e9f1541",
+    "url": "/phasercraft/static/js/2.c0e707df.chunk.js"
+  },
+  {
+    "revision": "f2d47e79505e94c828e3",
+    "url": "/phasercraft/static/js/main.f1c6e5ad.chunk.js"
+  },
+  {
+    "revision": "d9609019dd612e84c384",
+    "url": "/phasercraft/static/js/runtime~main.24b4b948.js"
+  }
+]);
