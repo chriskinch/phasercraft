bdd4e13c83aa5687922909e1d7e0c4939f55e0e0
Chris Kinch <christopher.kinch@lego.com>
2021-04-07 02:11:18 +0100

    Store actions

--- services/armory/items/delete.js
+++ services/armory/items/delete.js
@@ -2,38 +2,7 @@
 
 const dynamodb = require('./dynamodb');
 
-// module.exports.delete = (event, context, callback) => {
-//   const params = {
-//     TableName: process.env.DYNAMODB_TABLE,
-//     Key: {
-//       id: event.pathParameters.id,
-//     },
-//     ReturnValues: 'ALL_OLD',
-//   };  
-  
-//   // delete the todo from the database
-//   dynamodb.delete(params, (error) => {
-//     // handle potential errors
-//     if (error) {
-//       console.error(error);
-//       callback(null, {
-//         statusCode: error.statusCode || 501,
-//         headers: { 'Content-Type': 'text/plain' },
-//         body: 'Couldn\'t remove the todo item.',
-//       });
-//       return;
-//     }
-
-//     // create a response
-//     const response = {
-//       statusCode: 200,
-//       body: JSON.stringify({...params.Key}),
-//     };
-//     callback(null, response);
-//   });
-// };
-
-module.exports.delete = async (event, context, callback) => {
+module.exports.delete = async event => {
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
