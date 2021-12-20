8a6e6deee297e1c2d75df519bb3c9188de9ba8b0
Chris Kinch <christopher.kinch@lego.com>
2021-12-20 15:21:43 +0000

    Removed defined but unused imports

--- src/UI/components/templates/Armory.js
+++ src/UI/components/templates/Armory.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { connect } from "react-redux"
 import "styled-components/macro"
 import { pixel_emboss } from "@UI/themes"
