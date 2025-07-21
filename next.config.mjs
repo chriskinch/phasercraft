a77de9acae46bf019a8d7b2d0fd98083b277ad57
Christopher Kinch <christopher.kinch@lego.com>
2025-07-21 12:54:10 +0100

    Messing with paths and upload folders

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ const nextConfig = {
     images: {
         unoptimized: true, // Disable default image optimization
     },
-    assetPrefix: isProd ? '/phasercraft/dist/' : '',
-    basePath: isProd ? '/phasercraft/dist' : '',
+    assetPrefix: isProd ? '/phasercraft/' : '',
+    basePath: isProd ? '/phasercraft' : '',
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
