8862735e3410316c05ce869920f37cf49d93c5f2
Christopher Kinch <christopher.kinch@lego.com>
2025-07-21 09:41:23 +0100

    Clean up remaining chached capitalised folders

--- src/Helpers/targetVector.js
+++
@@ -1,5 +0,0 @@
-export default function targetVector(from, to){
-    const d = to.body.position.clone().subtract(from.body.position);
-    const range = Math.sqrt(Math.pow(d.x,2) + Math.pow(d.y,2));
-    return { range: range, delta: d };
-}
