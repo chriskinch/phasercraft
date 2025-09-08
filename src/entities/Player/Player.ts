768a34e7e139de88a26c48c0fade570bd6a83fc9
Christopher Kinch <christopher.kinch@lego.com>
2025-09-08 18:07:18 +0100

    Removed console.log

--- src/entities/Player/Player.ts
+++ src/entities/Player/Player.ts
@@ -73,7 +73,6 @@ class Player extends GameObjects.Container {
 		scene.add.existing(this);
 
 		this.body.collideWorldBounds = true;
-		console.log("Immovable: ", immovable);
 		this.body.immovable = immovable;
 		this.body.setFriction(0,0);
 
